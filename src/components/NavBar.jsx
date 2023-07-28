7414b1622421def01ec01715f7f9e082f28b20fc
Cristian Morales <cristianmorales3006@gmail.com>
2023-07-28 16:41:13 -0700

    Add sweetAlert - Remove label from btn

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,34 +2,34 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <nav className="navbar navbar-dark bg-dark">
-  <div className="container-md">
-    <a className="navbar-brand" href="/">Tattler App</a>
-    <button className="navbar-toggler  d-lg-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-      <div className="offcanvas-header">
-        <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">Tattler App</h5>
-        <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    <nav className="navbar navbar-dark bg-dark fixed-top">
+      <div className="container-md">
+        <a className="navbar-brand" href="/">Tattler App</a>
+        <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+          <div className="offcanvas-header">
+            <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">Tattler App</h5>
+            <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div className="offcanvas-body">
+            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="/">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">Guardados</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">Recientes</a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
-      <div className="offcanvas-body">
-        <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-          <li className="nav-item">
-            <a className="nav-link" aria-current="page" href="/">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/">Guardados</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/">Recientes</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </div>
-</nav>
-      );
-    };
+    </nav>
+  );
+};
 
 export default NavBar;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import RestaurantList from './RestaurantList';
+import swal from 'sweetalert';
+
 
 const SearchBar = () => {
   const [query, setQuery] = useState('');
@@ -24,10 +26,29 @@ const SearchBar = () => {
     }
   }, []);
 
+  useEffect(() => {
+    if (results) {
+      handleSearch(); 
+    }
+  }, [selectedFilter]);
+
+
   const handleSearch = async () => {
     const response = await fetch(`http://localhost:3001/search?engine=google_maps&q=${query}&latitude=${latitude}&longitude=${longitude}&openStateFilter=${selectedFilter}`);
     const data = await response.json();
+
+    // Actualizamos el estado con los nuevos resultados obtenidos de la búsqueda
     setResults(data);
+
+    if (data && data.local_results && data.local_results.length > 0) {
+      // Si hay resultados, mostramos hasta 3 restaurantes en la lista
+      setResults(data);
+    } else {
+      // Si no hay resultados, mostramos el mensaje de SweetAlert
+      swal("Sin resultados, intenta con otro filtro");
+      // Limpiamos los resultados para no mostrar la lista vacía
+      setResults(null);
+    }
   };
 
   return (
@@ -43,10 +64,9 @@ const SearchBar = () => {
         />
         <div className='btn-group'>
           <button onClick={handleSearch} className="btn btn-outline-success" type='button'>Buscar</button>
-          <div className='dropdown'>
-            <button id='filter-btn' type='button' className='btn btn-outline-success dropdown-toggle' data-bs-toggle='dropdown' data-bs-auto-close="outside" aria-expanded='false'>
-              {selectedFilter}
+          <button id='filter-btn' type='button' className='btn btn-outline-success dropdown-toggle' data-bs-toggle='dropdown' data-bs-auto-close="outside" aria-expanded='false'>
             </button>
+          <div className='dropdown'>
             <ul className='dropdown-menu'>
               <li className={`dropdown-item ${selectedFilter === 'Cualquier horario' ? 'active' : ''}`} onClick={() => setSelectedFilter('Cualquier horario')}>Cualquier horario</li>
               <li className={`dropdown-item ${selectedFilter === 'Abierto Ahora' ? 'active' : ''}`} onClick={() => setSelectedFilter('Abierto Ahora')}>Abierto Ahora</li>
