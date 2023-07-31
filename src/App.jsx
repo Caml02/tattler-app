84fe2beb72a6d493a43cbaab203a7fa2d21d9f49
Cristian Morales <cristianmorales3006@gmail.com>
2023-07-31 12:29:34 -0700

    Delete Errorcases, deleted unsuccessful code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { RestaurantProvider } from './context/RestaurantContext';
 import SideBar from './components/SideBar';
 import SearchBar from './components/SearchBar';
 import NavBar from './components/NavBar';
@@ -7,23 +6,22 @@ import Map from './components/Map';
 import './scss/App.scss';
 
 function App() {
+
   return (
-    <RestaurantProvider> 
-      <div>
-        <NavBar />
-        <div className="main">
-          <div className="sidebar-wrapper">
-            <SideBar />
-          </div>
-          <div className="searchbar-wrapper">
-            <SearchBar />
-          </div>
-          <div className="map-wrapper">
-            <Map />
-          </div>
+    <div>
+      <NavBar />
+      <div className="main">
+        <div className="sidebar-wrapper">
+          <SideBar />
+        </div>
+        <div className="searchbar-wrapper">
+          <SearchBar />
+        </div>
+        <div className="map-wrapper">
+          <Map />
         </div>
       </div>
-    </RestaurantProvider> 
+    </div>
   );
 }
 
